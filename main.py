87f784136be05dc5860fbed5588e8734d028eb0d
Siddhant Shirodkar <siddhant19shirodkar12@gmail.com>
2024-07-09 19:10:55 -0400

    Data Ingestion

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
 from src.secureloan import logger
+from src.secureloan.pipeline.stage_01_Data_ingestion import DataIngestionTrainingPipeline
 
-logger.info("Welcome to Custom log")
+
+STAGE_NAME = "Data Ingestion stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataIngestionTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
