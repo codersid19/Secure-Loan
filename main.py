c99453b2a13e3782dc8ec469796a780b17dcbc47
Siddhant Shirodkar <siddhant19shirodkar12@gmail.com>
2024-07-09 19:49:15 -0400

    Data Validation Included

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
 from src.secureloan import logger
 from src.secureloan.pipeline.stage_01_Data_ingestion import DataIngestionTrainingPipeline
+from src.secureloan.pipeline.stage_02_Data_validation import DataValidationTrainingPipeline
 
 
 STAGE_NAME = "Data Ingestion stage"
 try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
    data_ingestion = DataIngestionTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+
+STAGE_NAME = "Data Validation stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataValidationTrainingPipeline()
    data_ingestion.main()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
