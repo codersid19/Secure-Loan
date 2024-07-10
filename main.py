ce8ed76040d682ea8c7d4f524fd6dbb998c9345b
Siddhant Shirodkar <siddhant19shirodkar12@gmail.com>
2024-07-09 22:38:34 -0400

    Model Evaluation

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from src.secureloan.pipeline.stage_01_Data_ingestion import DataIngestionTrainin
 from src.secureloan.pipeline.stage_02_Data_validation import DataValidationTrainingPipeline
 from src.secureloan.pipeline.stage_03_Data_transformation import DataTransformationTrainingPipeline
 from src.secureloan.pipeline.stage_04_Model_trainer import ModelTrainerTrainingPipeline
-
+from src.secureloan.pipeline.stage_05_Model_evaluation import ModelEvaluationTrainingPipeline
 
 STAGE_NAME = "Data Ingestion stage"
 try:
@@ -48,4 +48,17 @@ try:
 except Exception as e:
         logger.exception(e)
         raise e
+
+
+STAGE_NAME = "Model evaluation stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = ModelEvaluationTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+
 
